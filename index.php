d6899ee0b42cb28e1f6987f1ddbfdf6bf86555c4
Iris <pradoirisheart_bscs@plmun.edu.ph>
2025-02-21 16:26:56 +0800

    New Update w/FE

--- index.php
+++ index.php
@@ -38,27 +38,65 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="index.css" rel="stylesheet">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-
+    <link href="dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <title>Login</title>
 </head>
+<style>
+    body{
+        background-color: #283618;
+    }
+
+    .login-box{
+        width: 400px;
+        height: 300px;
+
+        background-color: #f5f5f5;
+        border-radius: 10px;
+    }
+    #login{
+        margin: 10px;
+    }
+    .custom-input{
+        border-radius: 15px;
+        color: black;
+    }
+    .btn-login{
+        width:100%;
+        height:35px;
+
+        border-width: 1px;
+        border-radius: 20px;
+    }
+    .plmun_logo{
+        width: 120px;
+        height: 120px;
+    }
+
+</style>
 <body>
     <div class = "container">
-        <div class="position-absolute top-50 start-50 translate-middle">
+        <div class="position-absolute top-50 start-50 translate-middle login-box">
             <div id="login" >
-                <form method="POST" action="" class="form_login">
-                    <label>Username:</label>
-                    <input type="text" name="username" required>
-                    <br>
-                    <label>Pin:</label>
-                    <input type="password" name="pin" required>
-                    <br>
-                    <input type="submit" value="Login">
+                <form method="POST" action="" class="form_login row">
+                    <div class="col-12 d-flex justify-content-center align-items-center">
+                        <img src="plmun_logo.png" alt="plmun_logo" class="plmun_logo">
+                    </div>
+                    <div class="col-8 p-2 mx-auto">
+                        <input type="text" name="username" placeholder="USERNAME" class="form-control text-center custom-input" required>
+                    </div>
+                    <div class="col-8 p-2 mx-auto">
+                        <input type="password" name="pin" placeholder="PIN" class="form-control text-center custom-input" required>
+                    </div>
+                    <div class="col-5 mx-auto p-2">
+                        <button type="submit" class="btn-login">LOGIN</button>
+                    </div>
                 </form>
-
+            </div>
             <?php
             if (!empty($error_message)) {
                 echo "<p style='color: red;'>$error_message</p>";
@@ -66,5 +104,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             ?>
         </div>
     </div>
+
+
+
 </body>
+
 </html>
--- login/CITCS/BSIT/index.php
+++ login/CITCS/BSIT/index.php
@@ -4,18 +4,74 @@
     // Check if user is logged in
     if (isset($_SESSION['username'])) {
         $user_name = htmlspecialchars($_SESSION['username']);
-    } else {
-        $user_name = "Guest"; 
-    }
+    } 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>IT Document</title>
+    <title>IT</title>
+
+    <link href="/Root_1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="/Root_1/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
 </head>
+<style>
+    body{
+        margin: 0;
+        padding: 0;
+
+        font-family: 'Poppins';
+    }
+
+    .nav-bar{
+        width: 100%;
+        height: 40px;
+
+        margin: 0;
+        padding: 0;
+
+        background-color: #283618;
+    }
+   .logout{
+        display: flex;
+        justify-content: flex-end;
+        padding: 8px;
+    }
+    .btn-logout{
+        margin-right: 20px;
+
+        border: none;
+        border-radius: 10px;
+
+        width: 100px;
+        height: 25px;
+        background-color: #f5f5f5;
+    }
+    .user-display{
+        display: flex;
+        align-items: center;
+        padding-top: 8px;
+        padding-left: 30px;
+        font-size: 18px;
+        color: white;
+    }
+
+</style>
 <body>
-    <h1>Hello, <?php echo $user_name; ?>!</h1>
+    <div class="row nav-bar">
+        <div class="col-6 user-display">
+            <p>Hello, <?php echo $user_name; ?>!</h1>
+        </div>
+        <div class="col-6 logout">
+            <form action="/Root_1/logout.php" method="post">
+                <button type="submit" class="btn-logout">Logout</button>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
