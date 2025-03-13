e9ca44d46f4ea838b10d274d06ac467f9718bcc6
Iris Heart Prado <pradoirisheart_bscs@plmun.edu.ph>
2025-03-14 01:16:28 +0800

    New Update 0313

--- index.php
+++ index.php
@@ -4,167 +4,45 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="UTF-8">
 
+    <link rel="stylesheet" href="./index.css">
     <link href="dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="dist/js/bootstrap.bundle.min.js"></script>
     <title>Login</title>
 </head>
-<style>
-    /* Poppins Regular */
-    @font-face {
-        font-family: 'Poppins';
-        src: url('/Root_1/Poppins/Poppins-Regular.ttf') format('truetype');
-        font-weight: 400;
-        font-style: normal;
-    }
-
-    /* Poppins Bold */
-    @font-face {
-        font-family: 'Poppins';
-        src: url('/Root_1/Poppins/Poppins-Bold.ttf') format('truetype');
-        font-weight: 700;
-        font-style: normal;
-    }
-
-    /* Poppins Italic */
-    @font-face {
-        font-family: 'Poppins';
-        src: url('/Root_1/Poppins/Poppins-Italic.ttf') format('truetype');
-        font-weight: 400;
-        font-style: italic;
-    }
-
-    /* Poppins Light */
-    @font-face {
-        font-family: 'Poppins';
-        src: url('/Root_1/Poppins/Poppins-Light.ttf') format('truetype');
-        font-weight: 300;
-        font-style: normal;
-    }
-    * {
-	    margin: 0;
-	    padding: 0;
-	}
-    body{
-    	overflow-y: hidden;
-    }
-    .container-fluid{
-        padding:0;
-    }
-    /*Nav Bar Design START*/
-    .nav-bar{
-        position: absolute;
-        top: 0;
-        left: 0;
-
-        width: 100vw;
-        height: 40px;
-
-        margin: 0;
-        padding: 0;
-
-        background-color: #212529;
-    }
-    .email{
-    	display: flex;
-        justify-content: end; 
-        align-items: center;
-    }
-    .img-email{
-    	width: 20px;
-    	height: 20px;
-
-    	filter: brightness(0) saturate(100%) invert(75%) sepia(92%) saturate(400%) hue-rotate(-10deg);
-    }
-    .txt-email{
-    	color:#fdc500;
-    }
-    /*Nav Bar Design END*/
-
-	/*content design START*/
-    .overlay{
-        border-image-source: linear-gradient(hsl(150 100% 0% / 0.8),  
-        hsl(120 100% 0% / 0.8));
-        border-image-slice: fill 1;
-        
-    }
-	.contentSection{
-        background-image: url('img/plmun_rlrc.jpg');
-        background-size: cover;
-        background-repeat: no-repeat;
-
-        height:100vh;
-    }
-	.php-login{
-		padding: 0px;
-		margin-top: 60px;
-
-		display: flex;
-	    justify-content: center;
-	}
-
-    .login-box{
-        width: 400px;
-        height: 450px;
-        margin: 0;
-
-        background-color: #dce1de; /* Dark color with 40% opacity */
-        border-radius: 10px;
-    }
-    .custom-input{
-        border-radius: 15px;
-        color: white;
-    }
-    .btn-login{
-        width:100%;
-        height:35px;
-
-        border-width: 1px;
-        border-radius: 20px;
-    }
-    .plmun_logo_login{
-        margin-top: 60px;
-        width: 120px;
-        height: 120px;
-    }
-	/*content design END*/
-</style>
 <body>
-	<div class="container-fluid">
+	<div class="container-fluid main">
 		<!--Nav Section START-->
         <section class="navBarSection">
-            <div class="container-fluid">
-                <div class="row nav-bar">
-                    <div class="col-12 nav_btn">
-                        <div class="d-flex flex-row-reverse">
-                            <div class="p-2 txt-email">
-                                plmuncomm@plmun.edu.ph
-                            </div>
-                            <div class="p-2">
-                                <img src="img/email.png" alt="email" class="img-email">
-                            </div>
-                        </div>
-                    </div>
+            <div class="row">
+                <div class="col-2 col-md-1 p-0 d-flex align-items-center justify-content-center justify-content-md-end logo">
+                    <img src="img/plmun_logo.png" alt="logo" class="img-logo">
+                </div>
+                <div class="col-3 col-md-2 welcome p-0 ps-md-2 d-flex align-items-center">
+                    <span class="txt-welcome">WELCOME</span>
+                </div>
+                <div class="col-7 col-md-3 offset-md-6 d-flex align-items-center justify-content-end">
+                    <span class="txt-email align-items-center">plmuncomm@plmun.edu.ph</span>
                 </div>
             </div>
         </section>
         <!--Nav Section END-->
 
-        <!--Content Section START-->
-        <section class="contentSection overlay">
-            <div class="container-fluid">
-                <div class="row g-0">
-                    <div class="col-12 col-md-6 p-3 php-login d-flex">
+        <!-- Login and list of abstract START -->
+        <section class="lnlSection"> <!--lnl means login and list-->
+            <div class=row>
+                <div class="col-12 col-md-4 login d-flex justify-content-center">
                         <?php 
                             include 'login.php';
                         ?>
-                    </div>
-                    <div class="col-12 col-md-6 p-2">
-                        <!--LIST OF ABSTRACT-->
-                    </div>
+                </div>
+                <div class="col-12 col-md-8">
+                        
                 </div>
             </div>
         </section>
-        <!--Content Section END-->
+        <!-- Login and list of abstract END -->
+
+       
 	</div>
 </body>
 </html>
--- login.php
+++ login.php
@@ -4,19 +4,29 @@
     }
 ?>
 
-<div class="row login-box">
-    <div id="login" >
+<div class="row login-box p-0">
+    <div class="col-3 col-md-4 img-section p-0 m-0">
+        <img src="img/plmun_rlrc.jpg" alt="plmun_rlrc" class="img-plmun_rlrc">
+    </div>
+    <div class="col-9 col-md-8  d-flex justify-content-center align-items-center">
         <form method="POST" action="login_process.php" class="form_login row">
-            <div class="col-12 d-flex justify-content-center align-items-center pt-2">
-                <img src="img/plmun_logo.png" alt="plmun_logo_login" class="plmun_logo_login">
+            <div class="col-12">
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <span class="txt-plmun">PLMun</span>
+                    </div>
+                    <div class="col-12 text-center">
+                        <span class="txt-wVM">Where Values Matter</span>
+                    </div>
+                </div>
             </div>
-            <div class="col-8 p-2 mx-auto">
+            <div class="col-10 col-md-10 p-2 mx-auto">
                 <input type="text" name="username" placeholder="USERNAME" class="form-control text-center custom-input" required>
             </div>
-            <div class="col-8 p-2 mx-auto">
+            <div class="col-10 col-md-10 p-2 mx-auto">
                 <input type="password" name="pin" placeholder="PIN" class="form-control text-center custom-input" required>
             </div>
-            <div class="col-5 mx-auto p-2">
+            <div class="col-5 col-md-7 mx-auto p-2">
                 <button type="submit" class="btn-login">LOGIN</button>
             </div>
         </form>
